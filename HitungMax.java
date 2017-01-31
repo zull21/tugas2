9decdc224ffc01fe20d812eb79442cc6cccfbaab
Muhammad Azizul Jabbar <abu.abyan.alif@gmail.com>
2017-01-31 21:14:48 +0700

    Penambahan Koding HitungMax

---
+++ HitungMax.java
@@ -0,0 +1,15 @@
+public class HitungMax {
+    public static void main (String[] args){
+        int angka1 = 40;
+        int angka2 = 20;
+        int angka3 = 90;
+        int max = 0;
+        if (angka1 > max)
+            max = angka1;
+        if (angka2 > max)
+            max = angka2;
+        if (angka3 > max)
+            max = angka3;
+    System.out.println("Angka terbesar adalah " + max);
+    }
+}
